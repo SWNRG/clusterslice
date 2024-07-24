255aa9254b47d5824ab4dbe4ec8de15441281ed8
Lefteris Mamatas <emamatas@uom.edu.gr>
2024-07-24 08:01:55 +0000

    image repository fixes

--- controllers/execute_clusterslice_dhcp.sh
+++ controllers/execute_clusterslice_dhcp.sh
@@ -5,7 +5,7 @@ source common_scripts/configuration.sh
 
 # define default image_prefix, if it is not set
 if [[ -z "$image_prefix" ]]; then
-  image_prefix="swnrg"
+  image_prefix="swnuom"
 fi
 
 # remove existing clusterslice-dhcp, in the case it exists
